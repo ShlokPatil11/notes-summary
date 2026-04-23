e03f6a06e433754efca08ddafcd1bcdf9103750c
Soham <shlok814@gmail.com>
2026-04-23 23:51:31 +0530

    Fix API URL - point to correct backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://notes-summary-1.onrender.com/api',
+  baseURL: 'https://notes-summary.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
